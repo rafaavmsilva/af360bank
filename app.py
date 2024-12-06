0ba0b1af2a69309e5d1407131e0dc33eb6dcb67b
rafaavmsilva <rafaavmsilva@gmail.com>
2024-12-06 13:15:15 -0300

    Updated app.py to properly integrate all applications into one server

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'development_key')
+
+# Configure session
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['PERMANENT_SESSION_LIFETIME'] = 1800  # 30 minutes
+
+# Import and register blueprints
+from Comissoes.af360bank.app import app as comissoes_blueprint
+comissoes_blueprint.template_folder = os.path.join('Comissoes.af360bank', 'templates')
+comissoes_blueprint.static_folder = os.path.join('Comissoes.af360bank', 'static')
+app.register_blueprint(comissoes_blueprint, url_prefix='/comissoes')
+
+from financeiro.af360bank.app import app as financeiro_blueprint
+financeiro_blueprint.template_folder = os.path.join('financeiro.af360bank', 'templates')
+financeiro_blueprint.static_folder = os.path.join('financeiro.af360bank', 'static')
+app.register_blueprint(financeiro_blueprint, url_prefix='/financeiro')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/redirect/<project>')
-def redirect_to_subdomain(project):
-    if project == 'comissoes':
-        return redirect('https://sistema-de-comissoes.onrender.com/')
-    elif project == 'financeiro':
-        return redirect('https://projeto-financeiro.onrender.com/')
-    return redirect('/')
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
